4eb7c7338c313e06d737e2b8e7a3fb68883b3ace
SKhorramzadeh <s.khorramzadeh@gmail.com>
2024-10-13 20:07:14 +0330

    Add files via upload

---
+++ Phone.cs
@@ -0,0 +1,42 @@
+namespace Task_7_22
+{
+    internal class Program
+    {
+        //Task 1: Summation of two numbers.
+        static void Main(string[] args)
+        {
+		
+		Console.WriteLine("Enter phone number!");
+		for (;;;)
+		{
+		
+		string P = Console.ReadLine();
+		if (P.legth!=11)
+		{
+		Console.WriteLine("Enter phone number with 11 digit!");
+		countinue;
+		}
+		
+		P = Console.ReadLine();
+		char[] chars = P.ToCharArray();
+		if (char(1)!="0")
+		{
+		Console.WriteLine("Enter phone number with 0 start!");
+		countinue;
+		}
+		
+		
+		
+		break;
+		
+		}	
+		
+		
+        }
+		
+
+        }
+    }
+}
+
+
